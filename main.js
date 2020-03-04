65f42dc31527ed36959fa14b38ff846c1f15e8e2
Yoan Estrada <yoanestrada01@gmail.com>
2020-03-04 06:10:13 -0500

    Declaracion de objetos2

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
-//Cliente de EDTeam
+//Cliente Gym
 
-let na = "nom";
-let me = "bre";
-
-
-const tabla = {
-    [`${na}${me}`]: "Yoan",
-    
+const clienteGym = {
+    nombre: "Yoan",
+    edad: 28,
+    peso: 85,
+    altura: 175,
+    fechaRegistro: "22/02*/2020"
 }
 
-console.log(tabla);
+//Cliente Gym
+
+const clienteGym2 = {
+    nombre: "Alberto",
+    edad: 28,
+    peso: 95,
+    altura: 185,
+    fechaRegistro: "22/02/2020"
+}
+
+// Cliente Gym3
+const clienteGym3 = {
+    nombreCompleto: "Yoan Estrada",
+    edad: "25",
+    fechaDeRegistro: "22 de mayo de 2005"
+}
+
+console.log(clienteGym3);
+
+const clientes = [clienteGym, clienteGym2];
+
